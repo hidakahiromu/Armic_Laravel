e4aae62ad6731a6e589777c0d8eb3805240a091e
nissy <>
2020-12-03 18:33:21 +0900

    ログアウト機能の実装
    ログイン後のユーザーネームにマウスオーバーするとログアウトボタンを表示

--- Armic/resources/views/toppage.blade.php
+++ Armic/resources/views/toppage.blade.php
@@ -35,32 +35,29 @@
        <li><a><nobr>ニュース</nobr></a></li>
        
             <!--ログイン-->
-            <!---->
-            <li class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                              <nobr><a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a></nobr>
                             </li>
                           
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                           
+                            <li><nobr><a href="#">{{ Auth::user()->name }}<span>▼</span></a></nobr>
+                              <ul>
+                                <li><nobr><a href="{{ route('logout') }}"
+                                      onclick="event.preventDefault();
+                                          document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                                    </a></nobr></li>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
-                                </div>
+                              <ul>
+                          
                             </li>
+
                         @endguest
              </li>
        <!-- ログイン機能ここまで-->
