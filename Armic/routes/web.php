468b382ceedb349f2ee6fd571e7277768e086496
maclaichiya <maclaichiya1120@gmail.com>
2021-01-13 09:42:58 +0900

    全てrestfulcontrollerにするとバグが起こりました

--- Armic/routes/web.php
+++ Armic/routes/web.php
@@ -11,58 +11,43 @@
 |
 */
 /*toppage*/
-
-Route::get('/', function () {
-    return view('toppage');
-});
+/*return view('toppage')*/
+Route::resource('/', 'viewControllers\routesController');
 /*投稿された動画*/
-Route::get('/postmovie', function () {
-    return view('musiclist/Post_Page');
-});
-
+/*return view('musiclist/Post_Page');*/
+Route::resource('/postmovie', 'viewControllers\musiclist_Post_PageController');
 /*動画詳細*/
+/*return view('musiclist/playmusic/post-in')*/
+/*Route::resource('/postmovie/in', 'viewControllers\musiclist_Post_Page_inController');*/
 Route::get('/postmovie/in', function () {
     return view('musiclist/playmusic/post-in');
 });
 
 /*イベント*/
-Route::get('/event', function () {
-    return view('eventlist/eventlist');
-});
-Route::get('/artistlist', function () {
-    return view('artistlist/artistlist');
-});
-
-Route::get('/mypage', function () {
-    return view('mypage.Mypage');
-});
-Route::get('/mypage/Delete', function () {
-    return view('mypage.Mypage_Delete');
-});
-
-Route::get('/mypage/Musicpost', function () {
-    return view('mypage.Mypage_Music');
-});
-Route::get('/mypage/artistname', function () {
-    return view('mypage.MypageConfig');
-});
-
+/*return view('eventlist/eventlist')*/
+/*Route::resource('/event', 'EventController');*/
+Route::resource('/event/eventRegistration', 'EventRegistrationController');
+/*return view('artistlist/artistlist')*/
+Route::resource('/artistlist', 'viewControllers\artistlistController');
+/*return view('mypage.Mypage')*/
+Route::resource('/mypage', 'viewControllers\mypageController');
+/*return view('mypage.Mypage_Delete')*/
+Route::resource('/mypage/Delete', 'viewControllers\mypage_DeleteController');
+/*return view('mypage.Mypage_Music')*/
+Route::resource('/mypage/Musicpost', 'viewControllers\mypage_MusicpostController');
+/* return view('mypage.MypageConfig')*/
+Route::resource('/mypage/MypageConfig', 'viewControllers\mypage_MypageConfigController');
 /* other(Q&Aとか利用規約とか)*/
-Route::get('/q&a', function () {
-    return view('other/q&a');
-});
-Route::get('/terms', function () {
-    return view('other/terms');
-});
+/*return view('other/q&a')*/
+Route::resource('/q&a', 'viewControllers\q@aController');
+/*return view('other/terms')*/
+Route::resource('/terms', 'viewControllers\termsController');
 
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/event', 'EventController@index')->name('event');
 
-Route::resource('/event/eventRegistration', 'EventRegistrationController');
 
 
 /*
