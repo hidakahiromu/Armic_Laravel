89a95674b7e9884810ad9c51221f6d4d7138e6bd
日高 広夢 <chiro3330@i.softbank.jp>
2021-01-19 15:33:35 +0900

    チュートリアルの変更

--- Armic/resources/views/Tutorial.blade.php
+++ Armic/resources/views/Tutorial.blade.php
@@ -6,39 +6,6 @@
 @section('content')
 
 <body>
-  <header>
-
-    <div class="header-ul">
-      <!--左の要素-->
-      <ul class="header_list">
-        <li><a>
-            <nobr>ARMICについて</nobr>
-          </a></li>
-        <li><a>
-            <nobr>投稿ページ</nobr>
-          </a></li>
-        <li><a>
-            <nobr>アーティスト一覧</nobr>
-          </a></li>
-      </ul>
-      <!--ロゴ-->
-      <h1 class="header_logo"><a href="Top_Page.html" 　title="ARMIC">
-          <img src="armic__3_-removebg-preview.png" width="200" height="auto"></a>
-      </h1>
-      <!--右の要素-->
-      <ul class="header_list">
-        <li><a>
-            <nobr>イベント</nobr>
-          </a></li>
-        <li><a href="#new">
-            <nobr>ニュース</nobr>
-          </a></li>
-        <li><a href="Registration/Rogu.html">
-            <nobr>ログイン</nobr>
-          </a></li>
-      </ul>
-    </div>
-  </header>
 
   <div class="box">
     <h2>[チュートリアル]</h2>
@@ -53,11 +20,11 @@
     　　<br>
     <h3>ログインの方法</h3>
     <dd>まずはARMICにログインしましょう。<br>右上のログインボタンを押してください。</dd>
-    <img src="tuto1.png" class="" width="200px">
+    <img src="../image/tuto1.png" class="" width="200px">
     <dd>新規登録のボタンを押してください。</dd>
-    <img src="tuto2.png" class="" width="200px">
+    <img src="../image/tuto2.png" class="" width="200px">
     <dd>ユーザ名・メールアドレス・パスワードを設定しましょう。<br>最後に忘れずにチェックを入れましょう。</dd>
-    <img src="tuto3.png" class="" width="200px">
+    <img src="../image/tuto3.png" class="" width="200px">
     <dd>これであなたのログインは完了しました。</dd> <br>
     <h3>ログインユーザー</h3>
     <p>ログインユーザには機能が解放されます。</p>
@@ -70,29 +37,5 @@
   </div>
   <!--ページトップ-->
   <div id="page_top"><a href="#"></a></div>
-  <footer id="footer" class="outer-block">
-    <div class="inner-block">
-      <div class="wrap">
-        <div class="logo">
-
-          <h1>ARMIC</h1>
-          <p　class="sabu">
-            <nobr>音楽を届けるサイト</nobr>
-          </p>
-        </div>
-
-
-        <ul class="nav">
-          <li>ご利用に際して</li>
-          <li><a href="">Q&A</a></li>
-          <li><a href="">利用規約</a></li>
-        </ul>
-      </div>
-    </div>
-    </div>
-    <div class="copyright">
-      <p><small> © 2020 Armic</small></p>
-    </div>
-  </footer>
 </body>
 @endsection
