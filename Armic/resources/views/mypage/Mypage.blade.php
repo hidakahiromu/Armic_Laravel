69438fefc580e8de2471f2ad14992984ee1fe70d
maclaichiya <maclaichiya1120@gmail.com>
2021-01-19 09:23:13 +0900

    ひとまず完成の後少し修正
    修正

--- Armic/resources/views/mypage/Mypage.blade.php
+++ Armic/resources/views/mypage/Mypage.blade.php
@@ -19,29 +19,19 @@
 			<!--$usernameにusernameを入れる-->
 
 			<div class="Mypage_NameBox">
-				<p><?php echo $username; ?></p>
+				<p>{{ Auth::user()->name }}</p>
 				<p>さんようこそ</p>
 				<!--マイページ編集画面移動-->
 				<div class=Mypage_config><a href="/mypage/artistname"><button class="Mypage_Config_button" type="button" style="width:100%;height:100%">
-							<p>アーティスト名の取得</p>
+							<p>Config</p>
 						</button></a></div>
 			</div>
 
 			<div class="Mypage_IconBox">
 
-				<?php/*
 
-				$fp = fopen($_FILES['upimg']['tmp_name'], "rb");
-				$img = fread($fp, filesize($_FILES['upimg']['tmp_name']));
-				fclose($fp);
 
-				$enc_img = base64_encode($img);
 
-				$imginfo = getimagesize('data:application/octet-stream;base64,' . $enc_img);
-
-				echo '<img src="data:' . $imginfo['mime'] . ';base64,' . $enc_img . '">';
-*/
-				?>
 
 				<!-- <img src=$comment>画像ファイルの名前の変数を記入-->
 
@@ -69,9 +59,9 @@
 
 				<tr>
 					<th class='Mypage_Posted_MusicBox_TableNumber'>1</th>
-					<td class='Mypage_Posted_MusicBox_TableMusic'>イギリス人が喋ってる動画</td>
-					<td class='Mypage_Posted_MusicBox_TableTime'>0000/00/00</td>
-					<td class='Mypage_Posted_MusicBox_TableSter'>★★★★★</td>
+					<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 					<td class='Mypage_Posted_MusicBox_TableDelete'>
 						<div class='Mypage_MusicBox_Delete'><a href="/mypage/Delete"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 									<p>delete</p>
@@ -81,9 +71,9 @@
 
 				<tr>
 					<th class='Mypage_Posted_MusicBox_TableNumber'>2</th>
-					<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-					<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-					<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+					<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 					<td class='Mypage_Posted_MusicBox_TableDelete'>
 						<div class='Mypage_MusicBox_Delete'><a href="/mypage/Delete"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 									<p>delete</p>
@@ -91,9 +81,9 @@
 					</td>
 				<tr>
 					<th class='Mypage_Posted_MusicBox_TableNumber'>3</th>
-					<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-					<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-					<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+					<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 					<td class='Mypage_Posted_MusicBox_TableDelete'>
 						<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 									<p>delete</p>
@@ -102,9 +92,9 @@
 				</tr>
 				<tr>
 					<th class='Mypage_Posted_MusicBox_TableNumber'>4</th>
-					<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-					<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-					<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+					<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 					<td class='Mypage_Posted_MusicBox_TableDelete'>
 						<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 									<p>delete</p>
@@ -113,9 +103,9 @@
 				</tr>
 				<tr>
 					<th class='Mypage_Posted_MusicBox_TableNumber'>5</th>
-					<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-					<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-					<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+					<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+					<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 					<td class='Mypage_Posted_MusicBox_TableDelete'>
 						<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 									<p>delete</p>
@@ -133,9 +123,9 @@
 						<table class='Mypage_Input_Posted_MusicBox'>
 							<tr>
 								<th class='Mypage_Posted_MusicBox_TableNumber'>6</th>
-								<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+								<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 								<td class='Mypage_Posted_MusicBox_TableDelete'>
 									<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 												<p>delete</p>
@@ -144,9 +134,9 @@
 							</tr>
 							<tr>
 								<th class='Mypage_Posted_MusicBox_TableNumber'>7</th>
-								<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+								<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 								<td class='Mypage_Posted_MusicBox_TableDelete'>
 									<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 												<p>delete</p>
@@ -155,9 +145,9 @@
 							</tr>
 							<tr>
 								<th class='Mypage_Posted_MusicBox_TableNumber'>8</th>
-								<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+								<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 								<td class='Mypage_Posted_MusicBox_TableDelete'>
 									<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 												<p>delete</p>
@@ -166,9 +156,9 @@
 							</tr>
 							<tr>
 								<th class='Mypage_Posted_MusicBox_TableNumber'>9</th>
-								<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+								<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 								<td class='Mypage_Posted_MusicBox_TableDelete'>
 									<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 												<p>delete</p>
@@ -177,9 +167,9 @@
 							</tr>
 							<tr>
 								<th class='Mypage_Posted_MusicBox_TableNumber'>10</th>
-								<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-								<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+								<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+								<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 								<td class='Mypage_Posted_MusicBox_TableDelete'>
 									<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 												<p>delete</p>
@@ -205,9 +195,9 @@
 
 			<tr>
 				<th class='Mypage_Posted_MusicBox_TableNumber'>1</th>
-				<td class='Mypage_Posted_MusicBox_TableMusic'>音楽&リンク</td>
-				<td class='Mypage_Posted_MusicBox_TableTime'>投稿日時</td>
-				<td class='Mypage_Posted_MusicBox_TableSter'>★</td>
+				<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 				<td class='Mypage_Posted_MusicBox_TableDelete'>
 					<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 								<p>delete</p>
@@ -217,9 +207,9 @@
 
 			<tr>
 				<th class='Mypage_Posted_MusicBox_TableNumber'>2</th>
-				<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-				<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-				<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+				<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 				<td class='Mypage_Posted_MusicBox_TableDelete'>
 					<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 								<p>delete</p>
@@ -227,9 +217,9 @@
 				</td>
 			<tr>
 				<th class='Mypage_Posted_MusicBox_TableNumber'>3</th>
-				<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-				<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-				<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+				<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 				<td class='Mypage_Posted_MusicBox_TableDelete'>
 					<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 								<p>delete</p>
@@ -238,9 +228,9 @@
 			</tr>
 			<tr>
 				<th class='Mypage_Posted_MusicBox_TableNumber'>4</th>
-				<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-				<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-				<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+				<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 				<td class='Mypage_Posted_MusicBox_TableDelete'>
 					<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 								<p>delete</p>
@@ -249,9 +239,9 @@
 			</tr>
 			<tr>
 				<th class='Mypage_Posted_MusicBox_TableNumber'>5</th>
-				<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-				<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-				<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+				<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+				<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 				<td class='Mypage_Posted_MusicBox_TableDelete'>
 					<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 								<p>delete</p>
@@ -269,9 +259,9 @@
 					<table class='Mypage_Input_Posted_MusicBox'>
 						<tr>
 							<th class='Mypage_Posted_MusicBox_TableNumber'>6</th>
-							<td class='Mypage_Posted_MusicBox_TableMusic'>音楽&リンク</td>
-							<td class='Mypage_Posted_MusicBox_TableTime'>投稿日時</td>
-							<td class='Mypage_Posted_MusicBox_TableSter'>★</td>
+							<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 							<td class='Mypage_Posted_MusicBox_TableDelete'>
 								<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 											<p>delete</p>
@@ -281,9 +271,9 @@
 
 						<tr>
 							<th class='Mypage_Posted_MusicBox_TableNumber'>7</th>
-							<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-							<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-							<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+							<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 							<td class='Mypage_Posted_MusicBox_TableDelete'>
 								<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 											<p>delete</p>
@@ -291,9 +281,9 @@
 							</td>
 						<tr>
 							<th class='Mypage_Posted_MusicBox_TableNumber'>8</th>
-							<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-							<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-							<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+							<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 							<td class='Mypage_Posted_MusicBox_TableDelete'>
 								<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 											<p>delete</p>
@@ -302,9 +292,9 @@
 						</tr>
 						<tr>
 							<th class='Mypage_Posted_MusicBox_TableNumber'>9</th>
-							<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-							<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-							<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+							<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 							<td class='Mypage_Posted_MusicBox_TableDelete'>
 								<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 											<p>delete</p>
@@ -313,9 +303,9 @@
 						</tr>
 						<tr>
 							<th class='Mypage_Posted_MusicBox_TableNumber'>10</th>
-							<td class='Mypage_Posted_MusicBox_TableMusic'>a</td>
-							<td class='Mypage_Posted_MusicBox_TableTime'>a</td>
-							<td class='Mypage_Posted_MusicBox_TableSter'>a</td>
+							<td class='Mypage_Posted_MusicBox_TableMusic'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableTime'>-</td>
+							<td class='Mypage_Posted_MusicBox_TableSter'>-</td>
 							<td class='Mypage_Posted_MusicBox_TableDelete'>
 								<div class='Mypage_MusicBox_Delete'><a href="Mypage_Delete.php"><button class="Mypage_Delete_button" type="button" style="width:100%;height:100%">
 											<p>delete</p>
